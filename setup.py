d04ede5a7d2cfcc8cf9d6186651693f49f3e5074
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2019-03-25 16:46:03 +0100

    Version 1.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name             = 'autopaths',
-        version          = '1.1.0',
+        version          = '1.1.1',
         description      = 'autopaths is a python package for dealing with file paths and automation.',
         long_description = open('README.md').read(),
         license          = 'MIT',
