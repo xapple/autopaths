a02af516e4c318286dc6d4145d2211af70595c23
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2020-05-15 22:00:22 +0200

    Back to development version.

--- autopaths/__init__.py
+++ autopaths/__init__.py
@@ -1,5 +1,5 @@
 # Special variables #
-__version__ = '1.4.0'
+__version__ = '1.4.1'
 
 # Built in modules #
 import os
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name             = 'autopaths',
-        version          = '1.4.0',
+        version          = '1.4.1',
         description      = 'autopaths is a python package for dealing with file paths and automation.',
         long_description = open('README.md').read(),
         long_description_content_type = 'text/markdown',
