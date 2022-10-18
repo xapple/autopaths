259e65b806074b3d49b25e6ea6e2983fdbf13715
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-18 05:06:13 +0000

    Adding tarfile member sanitization to extractall()

--- autopaths/file_path.py
+++ autopaths/file_path.py
@@ -388,7 +388,26 @@ class FilePath(autopaths.base_path.BasePath):
         # Do it #
         import tarfile
         with tarfile.open(self.path, 'r:gz') as archive:
-            archive.extractall(path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(archive, path)
         # Return #
         return autopaths.dir_path.DirectoryPath(path + '/')
 
@@ -406,7 +425,26 @@ class FilePath(autopaths.base_path.BasePath):
         # Do it #
         import tarfile
         with tarfile.open(self.path) as archive:
-            archive.extractall(path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(archive, path)
         # Return #
         return autopaths.dir_path.DirectoryPath(path + '/')
 
